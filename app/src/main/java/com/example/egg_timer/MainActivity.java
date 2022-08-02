316810911f4c91011f674f0e02f4ba9e5ceebf67
gurpreet-singh25 <gpsingh5640@gmail.com>
2022-08-02 13:43:00 +0530

    timer-App

---
+++ app/src/main/java/com/example/egg_timer/MainActivity.java
@@ -0,0 +1,103 @@
+package com.example.egg_timer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView timerTextView;
+    SeekBar timerSeekBar;
+    Boolean counterIsActive = false;
+    Button goButton;
+    CountDownTimer countDownTimer;
+
+    public void resetTimer(){
+        timerTextView.setText("0:30");
+        timerSeekBar.setProgress(30);
+        timerSeekBar.setEnabled(true);
+        countDownTimer.cancel();
+        goButton.setText("GO!");
+        counterIsActive = false;
+    }
+
+    public void buttonClick(View view){
+        if (counterIsActive){
+            resetTimer();
+        }
+        else{
+
+
+        counterIsActive = true;
+        timerSeekBar.setEnabled(false);
+        goButton.setText("STOP!");
+            countDownTimer= new CountDownTimer(timerSeekBar.getProgress() *1000 + 100,1000){
+
+            @Override
+            public void onTick(long l) {
+                updateTimer((int)l/1000);
+
+            }
+
+            @Override
+            public void onFinish() {
+                MediaPlayer mplayer = MediaPlayer.create(getApplicationContext(),R.raw.bicycle_bell);
+                mplayer.start();
+                resetTimer();
+            }
+        }.start();}
+    }
+
+    public void updateTimer(int secondsLeft){
+        int minutes = secondsLeft / 60;
+        int seconds = secondsLeft - (minutes * 60) ;
+
+        String secondString = Integer.toString(seconds);
+        if(seconds <= 9){
+            secondString = "0"+ secondString;
+        }if(secondString.equals("0")){
+            secondString = "00";
+        }
+
+        timerTextView.setText(Integer.toString(minutes) + ":" + secondString );
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       timerSeekBar = (SeekBar) findViewById(R.id.timerSeekBar);
+         timerTextView = (TextView) findViewById(R.id.countdownTextView);
+         goButton =(Button) findViewById(R.id.goButtton);
+
+        timerSeekBar.setMax(600);
+        timerSeekBar.setProgress(30);
+
+        timerSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+               updateTimer(i);
+
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+    }
+}
